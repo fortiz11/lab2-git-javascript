7ce6f079e9c7faeb707a1ecc1617536ffcc7be5a
Francis Ortiz <fortiz@sandiego.edu>
2025-09-15 21:27:52 -0700

    feature: Scaffold strings module and add console tests to main file

--- index.js
+++ index.js
@@ -6,6 +6,7 @@
 
 // Import math functions 
 import * as math from './math.js';
+import * as strings from './strings.js';
 
 console.log('=== Testing math module ===');
 console.log('add(5,3)=',math.add(5,3));
@@ -15,3 +16,10 @@ console.log('divide(20,4)=', math.divide(20,4));
 console.log('power(2,8)=', math.power (2,8));
 console.log('sqrt(16)=', math.sqrt(16));
 console.log('factorial(5)=', math.factorial(5));
+
+console.log("\n== Testing String Module ==");
+console.log("reverse('hello') =", strings.reverse("hello"));
+console.log("capitalize('javascript') =", strings.capitalize("javascript"));
+console.log("countVowels('hello world') =", strings.countVowels("hello world"));
+console.log("isPalindrome('racecar') =", strings.isPalindrome("racecar"));
+console.log("isPalindrome('hello') =", strings.isPalindrome("hello"));
---
+++ strings.js
@@ -0,0 +1,45 @@
+/**
+ * String utilities for learning purposes 
+ * @file 'strings.js'
+ * @author 'Francis Ortiz'
+ */
+
+/**
+ * Reverse a string 
+ * @param {string} str - String to reverse 
+ * @returns {string} Reversed string
+ */
+
+export function reverse(str){
+    return "stubbed";
+}
+
+/**
+ * Capitalize the first letter of a string 
+ * @param {string} str - String to capitalize
+ * @returns {string} Capitalized string 
+ */
+
+export function capitalize (str){
+    return "stubbed";
+}
+
+/**
+ * Count vowels in a string 
+ * @param {string} str - String to count vowels in 
+ * @returns {number} Number of vowels 
+ */
+
+export function countVowels (str){
+    return "stubbed";
+}
+
+/**
+ * check if a string is a palindrome 
+ * @param {string} str - String to check 
+ * @returns {boolean} True if palindrome
+ */
+
+export function isPalindrome (str){
+    return "stubbed";
+}
