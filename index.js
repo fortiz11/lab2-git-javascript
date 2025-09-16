9fcf521b59a96a9f3471305d49f0053164761e00
Francis Ortiz <fortiz@sandiego.edu>
2025-09-15 21:06:38 -0700

    feat: Created index.js to test math module

---
+++ index.js
@@ -0,0 +1,17 @@
+/**
+ * Main entry point for Lab 2 
+ * @file 'index.js'
+ * @author 'Francis Oritz'
+ */
+
+// Import math functions 
+import * as math from './math.js';
+
+console.log('=== Testing math module ===');
+console.log('add(5,3)=',math.add(5,3));
+console.log('subtract(10,4=', math.subtract (10,4));
+console.log('multiply(3,7)=', math.multiply (3,7));
+console.log('divide(20,4)=', math.divide(20,4));
+console.log('power(2,8)=', math.power (2,8));
+console.log('sqrt(16)=', math.sqrt(16));
+console.log('factorial(5)=', math.factorial(5));
